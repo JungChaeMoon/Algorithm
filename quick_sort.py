3d9b9c60e02f64188da0c6f50fb916264630fa21
JungChaeMoon <jungchaemoon@gmail.com>
2020-08-27 23:24:03 +0900

    퀵소트

---
+++ quick_sort.py
@@ -0,0 +1,29 @@
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+
+        while left <= end and array[left] <= array[pivot]:
+            left = left + 1
+
+        while start < right and array[pivot] <= array[right]:
+            right = right - 1
+
+        if left > right:
+            array[pivot], array[right] = array[right], array[pivot]
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+
+
+quick_sort(array, 0, len(array)-1)
+print(array)
+
