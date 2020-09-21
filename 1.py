3276a1b7755fbbeee0bd1777ed8b6b975c65aa2e
JungChaeMoon <jungchaemoon@gmail.com>
2020-09-21 22:48:16 +0900

    1로만들기

---
+++ 1.py
@@ -0,0 +1,16 @@
+x = int(input())
+
+dp = [0] * 30001
+
+for i in range(2, x+1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 5 == 0:
+        dp[i] = min(dp[i], dp[i // 5] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
+print(dp[x])
+
