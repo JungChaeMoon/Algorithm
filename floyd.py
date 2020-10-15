d523847718db7e3a1c0afbe234dad7ea2e1ff910
JungChaeMoon <jungchaemoon@gmail.com>
2020-10-16 00:39:37 +0900

    플로이드 워셜 알고리즘

---
+++ floyd.py
@@ -0,0 +1,29 @@
+import sys
+
+INF = int(1e9)
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print('inf', end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print('\n')
