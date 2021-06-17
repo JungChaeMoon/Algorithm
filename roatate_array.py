649f3c3a77e09cd9c69892947d3299f832b6b3ac
JungChaeMoon <jungchaemoon@gmail.com>
2021-06-18 01:15:19 +0900

    Add solution

--- roatate_array.py
+++ roatate_array.py
@@ -7,3 +7,8 @@ def rotate(nums, k):
         del_num = deq.pop()
         deq.appendleft(del_num)
     nums[:] = list(deq)
+
+
+def rotate_slice(nums, k):
+    k = k % len(nums)
+    nums[:] = nums[-k:] + nums[:-k]
