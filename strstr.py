2fe78b87744c57dee076df6edc1de13485110afb
JungChaeMoon <jungchaemoon@gmail.com>
2021-07-06 23:19:58 +0900

    update strstr

--- strstr.py
+++ strstr.py
@@ -1,9 +1,18 @@
 def strstr(haystack, needle):
-    index = haystack.find(needle)
-    if index == -1:
+    if needle == "":
         return 0
-    else:
-        return index
+    return haystack.find(needle)
+
+
+def strstr2(haystack, needle):
+    if needle == "":
+        return 0
+    needle_len = len(needle)
+    for i in range(len(haystack)):
+        if haystack[i: i + needle_len] == needle:
+            return i
+    return -1
 
 
 print(strstr(haystack = "aaaaa", needle = "bba"))
+print(strstr2(haystack = "hello", needle = "ll"))
