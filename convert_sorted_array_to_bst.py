73b40bc0d4c7ce004bf8a7e8d41076bcd8d09e8b
JungChaeMoon <jungchaemoon@gmail.com>
2021-07-25 14:40:14 +0900

    update

--- convert_sorted_array_to_bst.py
+++ convert_sorted_array_to_bst.py
@@ -28,3 +28,30 @@ class Solution:
         for num in nums[length + 1:]:
             insert(num, root)
         return root
+
+
+import sys
+
+sys.setrecursionlimit(150000)
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None
+                 ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return node
