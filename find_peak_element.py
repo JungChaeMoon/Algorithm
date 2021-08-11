99244d14303277dcaceff9ba0cf529e192a6e1c4
JungChaeMoon <jungchaemoon@gmail.com>
2021-08-11 16:37:46 +0900

    find peak element

---
+++ find_peak_element.py
@@ -0,0 +1,12 @@
+def find_peak_element(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+
+print(find_peak_element([1, 2, 1, 4, 5, 6, 4]))
